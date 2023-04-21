69b50cbdb9a139239310140d7372673efda72dab
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-04-21 19:13:35 +0530

    pushing the code into github repo

---
+++ index.js
@@ -0,0 +1,11 @@
+let background_color =document.body;
+let  text= document.getElementById("tool-tip")
+function backgroundonchange(){
+        background_color.classList.toggle("first")
+        if(body.background_coloris$("Black")){
+            return text="white"
+        }else{
+            return text ="Black"
+        }
+        
+}
